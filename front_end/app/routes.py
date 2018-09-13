0d655336022221bfae4b51be7b87dd04590d48a0
jmwashi <jmwashi@bu.edu>
2018-09-13 14:47:35 -0400

    Updated Log In Page

--- front_end/app/routes.py
+++ front_end/app/routes.py
@@ -1,18 +1,17 @@
 from app import app
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
     return '''
 <html>
     <head>
-        <title>Home Page - Microblog</title>
+        <title>Home Page - Sensor Site Senior Design</title>
     </head>
     <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
+        <h1>Hello!</h1>
     </body>
 </html>'''
 
